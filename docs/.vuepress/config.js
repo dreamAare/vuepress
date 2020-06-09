618054710a06c912baea2555bd809402d17e0946
刘任志 <renzhi.liu@iinda.cn>
2020-06-09 16:59:15 +0800

    vuepress 基本用法

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: '刘任志',
+  description: '刘任志',
+  themeConfig: {
+    nav: [{
+        text: '主页',
+        link: '/'
+      },
+      {
+        text: '测试',
+        link: '/test.md'
+      },
+      {
+        text: '百度',
+        link: 'https://www.baidu.com'
+      },
+    ],
+    sidebar: [
+      ['/test1.md', '测试1'],
+      ['/test2.md', '测试2'],
+    ]
+  }
+}
